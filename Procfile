8394c9a6111c51ad1c31b33ecbf82359258f4714
Ladeologun <ologunomolade21@gmail.com>
2021-08-20 16:21:27 +0100

    updated settings for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn zuricv.wsgi --log-file -
--- zuricv/settings.py
+++ zuricv/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zzbqvff8tvde1z)sqiy7i8depul-jne&t^v-jwtuh_s*_iqc^k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['plugzuricv.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
